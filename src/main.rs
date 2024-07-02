6f6ec48e2cadda9efb8d34e5bb249ec1b94e6a9d
James-wasonga <jameswasonga22@gmail.com>
2024-07-02 16:55:35 +0300

    connecting the files

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,33 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+use minigrep::run;
+
+use minigrep::Config;
+
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // let query = &args[1];
-    // let file_path = &args[2];
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem passing an arguments: {err}");
-        process::exit(1);
-    });
+    if let Err(e) = minigrep::run(config) {
+        let args: Vec<String> = env::args().collect();
+        // let query = &args[1];
+        // let file_path = &args[2];
+        let config = Config::build(&args).unwrap_or_else(|err| {
+            println!("Problem passing an arguments: {err}");
+            process::exit(1);
+        });
+        
+        println!("Searching for {}",config.query);
+        println!("In file {}",config.file_path);
     
-    println!("Searching for {}",config.query);
-    println!("In file {}",config.file_path);
-
-//     let contents = fs::read_to_string(config.file_path)
-//         .expect("Should have been able to read the file");
-//     println!("with text:\n {}",contents);
-
-
-//calling the run function here
- if let Err(e) = run(config){
-    println!("Application error: {e}");
-    process::exit(1);
- }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-    // .expect("Should have been able to read the file");
-    println!("with text:\n {}",contents);
-
-    Ok(())
-}
-struct Config{
-    query: String,
-    file_path: String,
-}
-impl Config{
-    fn build(args: &[String]) ->Result<Config, &'static str> {
-        if args.len() < 3{
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config{query, file_path})
+    //     let contents = fs::read_to_string(config.file_path)
+    //         .expect("Should have been able to read the file");
+    //     println!("with text:\n {}",contents);
+    
+    
+    //calling the run function here
+     if let Err(e) = run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+     }
     }
+    
 }
-// fn parse_config(args: &[String]) -> Config{
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
-
-//     Config{query, file_path}
-// }
